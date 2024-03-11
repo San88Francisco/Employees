7b71dcee183b4884c34324c79189a2c1d59828e7
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-03-11 22:51:34 +0100

    add slice

--- client/src/pags/login/login.tsx
+++ client/src/pags/login/login.tsx
@@ -1,17 +1,40 @@
+import {useState} from 'react'
 import {Layout} from '../../components/layout/layout'
 import {Card, Form, Row, Space, Typography} from 'antd'
 import CustomInput from '../../components/custom-input/input'
 import PasswordInput from '../../components/password-input/password-input'
 import CustomBtn from '../../components/custom-btn/btn'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {Paths} from '../../paths'
+import {UserData, useLoginMutation} from '../../app/services/auth'
+import {isErrorWithMessage} from '../../utils/is-error-with-message'
+import ErrorMessage from '../../components/error-message/error'
 
 const Login = () => {
+  const navidate = useNavigate()
+  const [loginUser, loginUserResult] = useLoginMutation()
+  const [error, setError] = useState('')
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap()
+      navidate('/')
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err)
+
+      if (maybeError) {
+        setError(err.data.message)
+      } else {
+        setError('Невідома помилка')
+      }
+    }
+  }
+
   return (
     <Layout>
       <Row align='middle' justify='center'>
         <Card title='Війти' style={{width: '30rem'}}>
-          <Form onFinish={() => null}>
+          <Form onFinish={login}>
             <CustomInput type='email' name='email' placeholder='Email' />
             <PasswordInput name='password' placeholder='Password' />
             <CustomBtn type='primary' htmlType='submit'>
@@ -22,6 +45,7 @@ const Login = () => {
             <Typography.Text>
               Немає акаунта? <Link to={Paths.register}> Зареєструватись</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
