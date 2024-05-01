edf6555548b5d4ef936cb1c78f20c2c08bd3f5ce
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-05-01 22:20:01 +0200

    add router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {ConfigProvider, theme} from 'antd'
-import {createRoot} from 'react-dom/client'
-import {Provider} from 'react-redux'
-import {store} from './app/store'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { ConfigProvider, theme } from 'antd'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
-import {Paths} from './paths'
+import { Paths } from './paths'
 import './index.scss'
 import Login from './pags/login/login'
 import Register from './pags/register/register'
@@ -16,54 +16,55 @@ import Status from './pags/status/status'
 import Employee from './pags/employee/employee'
 import EditEmployee from './pags/edit-employee/edit-employee'
 
+//router
 const router = createBrowserRouter([
-  {
-    path: Paths.home,
-    element: <Employees />,
-  },
-  {
-    path: Paths.login,
-    element: <Login />,
-  },
-  {
-    path: Paths.register,
-    element: <Register />,
-  },
-  {
-    path: Paths.employeeAdd,
-    element: <AddEmployee />,
-  },
-  {
-    path: `${Paths.status}/:status`,
-    element: <Status />,
-  },
-  {
-    path: `${Paths.employee}/:id`,
-    element: <Employee />,
-  },
-  {
-    path: `${Paths.employeeEdit}/:id`,
-    element: <EditEmployee />,
-  },
+   {
+      path: Paths.home,
+      element: <Employees />,
+   },
+   {
+      path: Paths.login,
+      element: <Login />,
+   },
+   {
+      path: Paths.register,
+      element: <Register />,
+   },
+   {
+      path: Paths.employeeAdd,
+      element: <AddEmployee />,
+   },
+   {
+      path: `${Paths.status}/:status`,
+      element: <Status />,
+   },
+   {
+      path: `${Paths.employee}/:id`,
+      element: <Employee />,
+   },
+   {
+      path: `${Paths.employeeEdit}/:id`,
+      element: <EditEmployee />,
+   },
 ])
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ConfigProvider
-        theme={{
-          algorithm: theme.darkAlgorithm,
-        }}
-      >
-        <Auth>
-          <RouterProvider router={router}></RouterProvider>
-        </Auth>
-      </ConfigProvider>
-    </Provider>
-  </React.StrictMode>
+   <React.StrictMode>
+      <Provider store={store}>
+         <ConfigProvider
+            theme={{
+               algorithm: theme.darkAlgorithm,
+            }}
+         >
+            <Auth>
+               <RouterProvider router={router}></RouterProvider>
+            </Auth>
+         </ConfigProvider>
+      </Provider>
+   </React.StrictMode>
 )
 
 reportWebVitals()
